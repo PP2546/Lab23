3418b38a37295280d5dc15909078cdfc8746510a
PP2546 <phiphatasia@gmail.com>
2023-03-03 16:10:02 +0700

    Full

--- average.cpp
+++ average.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+#include<cstdlib>
 
+using namespace std;
+
+int main(int argc , char *argv[])
+{
+    float a;
+    float sum = 0;
+    if(argc == 1) cout << "Please input numbers to find average.";
+    else{
+        for(int i=1 ; i < argc ; i++)
+        {
+            a = atoi(argv[i]);
+            sum += a;
+        }
+        cout << "---------------------------------\n";
+        cout << "Average of " << argc-1 << " numbers = " <<  sum/(argc-1) ;
+        cout << "\n---------------------------------";
+    }
+}
